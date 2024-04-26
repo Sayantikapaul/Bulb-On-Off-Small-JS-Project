b86d001ed3a96c06b480357092abfd2cd96b42e1
Sayantika Paul <121125983+Sayantikapaul@users.noreply.github.com>
2024-04-26 22:21:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function lightOn(){
+  const image=document.getElementById('image');
+  image.src="lightonimage.png";
+  image.width='150rem'; 
+}
+function lightOff(){
+  const image=document.getElementById('image');
+  image.src="lightoffimage.png"; 
+}
